d644d4f74ba6b6d00c5c9a53ce9bc9ec69365885
Sameer Ghanate <sameerghanate@gmail.com>
2024-10-07 10:40:48 +0000

    First Commit

---
+++ src/main/java/com/example/GitActionsdemo/GitActionsdemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.GitActionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionsdemoApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/example/GitActionsdemo/GitActionsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitActionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
